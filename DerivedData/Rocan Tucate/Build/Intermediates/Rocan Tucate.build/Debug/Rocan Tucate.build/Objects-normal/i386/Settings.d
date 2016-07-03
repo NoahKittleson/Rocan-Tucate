fc48fe3fad21beb8cc4a133074638aaeaa2e6b8d
Noah Kittleson <noahkittleson@gmail.com>
2016-07-02 21:46:38 -0700

    removed DerivedData from being tracked

--- DerivedData/Rocan Tucate/Build/Intermediates/Rocan Tucate.build/Debug/Rocan Tucate.build/Objects-normal/i386/Settings.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/noahkittleson/Desktop/Rocan-Tucate/Rocan\ Tucate/Settings.cpp \
-  /Users/noahkittleson/Desktop/Rocan-Tucate/Rocan\ Tucate/Settings.h
--- DerivedData/Rocan Tucate/Build/Intermediates/Rocan Tucate.build/Debug/Rocan Tucate.build/Objects-normal/x86_64/ResourcePath.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/noahkittleson/Desktop/Rocan-Tucate/Rocan\ Tucate/ResourcePath.mm \
-  /Users/noahkittleson/Desktop/Rocan-Tucate/Rocan\ Tucate/ResourcePath.hpp
